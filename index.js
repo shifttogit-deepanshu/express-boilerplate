74cc0e8d1742544d01c84b7ecd3ad13bde6782c5
shifttogit-deepanshu <sharmadeepanshu.kaizen1@gmail.com>
2022-05-20 01:51:06 +0530

    init commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.get("/",(req,res)=>{
+    console.log("Hello World!")
+    res.send("Hello World")
+})
+
+app.listen(port,()=>{
+    console.log("App Listening on port http://localhost:"+port)
+})
